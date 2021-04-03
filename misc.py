759857c34d26e355664ad147cab8052bfd3e86f9
Anjana Madushanka <71401231+Anjana-Ma@users.noreply.github.com>
2021-04-03 18:32:20 +0530

    Update misc.py

--- misc.py
+++ misc.py
@@ -10,7 +10,7 @@ HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
 
 __**NOTE: Do Not Assign These Commands To Bot Via BotFather.**__"""
 
-START_TEXT = "__**Hi I'm Telegram Voice Chat Bot. Join @PatheticProgrammers For Support.**__"
+START_TEXT = "__**Hi I'm Telegram Voice Chat Bot. Join @Telegram For Support.**__"
 
 REPO_TEXT = "[Github](https://github.com/thehamkercat/Telegram_vc_bot)" \
-            + " | [Group](t.me/PatheticProgrammers)"
+            + " | [Group](t.me/Telegram)"
